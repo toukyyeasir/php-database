5832fd87fd4fe3806cfa1590e5b3425e48655742
toukyyeasir <78078359+toukyyeasir@users.noreply.github.com>
2021-03-30 17:24:37 +0600

    first commit

---
+++ registration.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Registration Form</title>
+    </head>
+    <body>
+        <?php
+
+            $firstname = $lastname = $gender = $email = $username = $password = $rec_email ="";
+            $firstnameerr = $lastnameerr= $gendererr = $emailerr = $usernameerr = $passworderr = $rec_emailerr = $notavailable = "";
+
+            if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+                if(empty($_POST['fname'])) {
+                    $firstnameerr = "Please Fill up the firstname!";
+                }
+
+                else if(empty($_POST['lname'])) {                    
+                    $lastnameerr = "Please Fill up the lastname!";
+                    
+                }
+
+                else if(empty($_POST['gender'])) {                    
+                    $gendererr = "Please Fill up the gender!";
+                    
+                }
+
+                else if(empty($_POST['email'])) {                    
+                    $emailerr = "Please Fill up the email!";
+                    
+                }
+
+                else if(empty($_POST['uname'])) {                    
+                    $usernameerr = "Please Fill up the username!";
+                }
+
+                else if(empty($_POST['pass'])) {                    
+                    $passworderr = "Please Fill up the password!";
+                }
+
+                else if(empty($_POST['remail'])) {                    
+                    $rec_emailerr = "Please Fill up the recovery email!";
+                }
+
+                else {
+
+                    $firstname = $_POST['fname'];
+                    $lastname = $_POST['lname'];
+                    $gender = $_POST['gender'];
+                    $email = $_POST['email'];
+                    $username = $_POST['uname'];
+                    $password = $_POST['pass'];
+                    $rec_email = $_POST['remail'];
+        
+
+
+
+
+                    
+                            // $_SESSION['message'] = "Successfull";
+
+                            // header('Location: login.php');
+
+
+
+
+
+
+
+
+                    }
+            }
+        ?>
+
+        <h1>Registration Form</h1>
+        <form  action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+
+
+
+            <fieldset>
+                <legend><b>Basic Information:</b></legend>
+            
+                <label for="firstname">First Name:</label>
+                <input type="text" name="fname" id="firstname">
+                <?php echo $firstnameerr; ?>
+
+                <br>
+
+                <label for="lastname"> LastName:</label>
+                <input type="text" name="lname" id="lastname">
+                <?php echo $lastnameerr; ?>
+
+                <br>
+
+                <label for="gender">Gender:  </label>
+                <input type="radio" name="gender" id="male" value="male">  
+                <label for="gender">Male </label>
+                <input type="radio" name="gender" id="female" value="female">
+                <label for="gender">Female </label>
+                <input type="radio" name="gender" id="other" value="other">
+                <label for="gender">Other </label>
+                <?php echo $gendererr; ?>
+
+                <br>
+
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email">
+                <?php echo $emailerr; ?>
+
+            </fieldset>
+
+            <fieldset>
+                <legend><b>Account Information:</b></legend>
+
+                <label for="username">Username:</label>
+                <input type="text" name="uname" id="username">
+                <?php echo $usernameerr; echo $notavailable; ?>
+
+                <br>
+
+                <label for="parmanent_address">Password:</label>
+                <input type="password" name="pass" id="password">
+                <?php echo $passworderr; ?>
+
+                <br>
+
+                <label for="rec_email">Recovery email:</label>
+                <input type="email" id="rec_email" name="remail">
+                <?php echo $rec_emailerr; ?>
+                
+                </fieldset>
+
+            <input type="submit" value="Submit" > 
+             <?php  
+    $hostname = "localhost";
+    $userName = "task";
+    $password = "123";
+    $dbname = "wtj";
+
+    // Mysqli Object-Oriented
+    $conn1 = new mysqli($hostname, $userName, $password, $dbname);
+    if($conn1->connect_errno) {
+        echo "<br>";
+        echo "1. Database Connection Failed!...";
+        echo "<br>";
+        echo $conn1->connect_error;
+    }
+    else {
+        echo "<br>";
+        echo "1. Database Connection Successful!";
+
+        $sql1 = "insert into user (firstName, lastName, Gender, email, userName, password, recoEmail) values ('$firstname', '$lastname', '$gender', '$email', '$username', '$password', '$rec_email')";
+        if ($conn1->query($sql1)){
+            echo "Data Inserted successfully";
+        }
+        else{
+            echo "Falied To Insert Data";
+            echo "<br>";
+            echo $conn1->error;
+        }
+    }
+
+    echo "<br>";
+
+
+
+
+
+
+            ?>
+        </form>
+        
+    </body>
+</html>
+
+
