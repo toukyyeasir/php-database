395c53024a909e8cb08da54f435560569223926f
toukyyeasir <78078359+toukyyeasir@users.noreply.github.com>
2021-04-05 17:48:56 +0600

    first commit

---
+++ cssLayout.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JS Form</title>
+	<style>
+.header {
+  border-style: solid;
+  border-color: black;
+  background-color: lightblue;
+  text-align: center;
+  height: 100px;
+  padding: 10px;
+   float: top;
+
+
+}
+.nav {
+	border-style: solid;
+
+	height: 540px;
+	width: 19.35%;
+	position: left;
+	background-color: cadetBlue;
+	text-align: center;
+	float: left;
+}
+
+.nav a:hover{
+	display: block;
+	background-color: cornSilk;
+  	color: black;
+}
+
+.content {
+	border-style: solid;
+	height: 540px;
+	width: 79%;
+	position: right;
+	background-color: cornSilk;
+	text-align: center;
+	float: right;
+}
+
+
+.footer {
+	border-style: solid;
+  background-color: lightsteelBlue;
+ 
+  padding: 10px;
+  text-align: center;
+}
+ul {
+  list-style-type: square;
+  margin: 0;
+  padding: 0;
+  font-size: 30px;
+}
+
+</style>
+</head>
+<body>
+	<div class="header">
+		<h1> Welcome </h1>	
+	</div>
+
+
+	<div class="nav">
+
+ 	<ul>
+  		<li><a href="">Home</a></li>
+  		<li><a href="">  News</a></li>
+  		<li><a href="">Contact</a></li>
+  		<li><a href="">About</a></li>
+	</ul> 
+		
+	</div>
+
+	<div class="content">
+
+		<h1> COVID-19 </h1>	
+		<p align="left" >Coronavirus disease 2019 (COVID-19) is a contagious disease caused by severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2). The first case was identified in Wuhan, China, in December 2019.[7] The disease has since spread worldwide, leading to an ongoing pandemic.[8]
+
+			<br>
+			<br>
+
+Symptoms of COVID-19 are variable, but often include fever, cough, fatigue, breathing difficulties, and loss of smell and taste. Symptoms may begin one to fourteen days after exposure to the virus. At least a third of people who are infected do not develop noticeable symptoms.[9] Of those people who develop noticeable symptoms, most (81%) develop mild to moderate symptoms (up to mild pneumonia), while 14% develop severe symptoms (dyspnea, hypoxia, or more than 50% lung involvement on imaging), and 5% suffer critical symptoms (respiratory failure, shock, or multiorgan dysfunction).[10] Older people are more likely to have severe symptoms. Some people continue to experience a range of effects—known as long COVID—for months after recovery, and damage to organs has been observed.[11] Multi-year studies are underway to further investigate the long-term effects of the disease.[11]
+</p>
+	</div>
+
+
+
+	<div class="footer">
+			<h1> &#169 Copyright 2021 </h1>	
+	</div>
+
+	
+
+</body>
+</html> 
